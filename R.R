553749a89086152bf883f1725a2d334c49b7d899
NutsCondensedMilk <72687472+NutsCondensedMilk@users.noreply.github.com>
2022-06-23 22:13:01 +0700

    Add files via upload

---
+++ R.R
@@ -0,0 +1,11 @@
+library("lubridate") # работа с датами
+library("zoo") # временные ряды
+library("dplyr") # манипуляция с данными
+library("ggplot2") # графики
+library("forecast") # прогноз
+library("xts") # расширение для zoo
+y <- AirPassengers
+mod_a <- auto.arima(y)
+summary(mod_a)
+prognoz_a <- forecast(mod_a, h=25)
+plot(prognoz_a)
